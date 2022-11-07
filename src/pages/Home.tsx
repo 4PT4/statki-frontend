5f4bd66cd50c6c802729d0cc46080ff89ba0c82a
Maciej <maciejas2003@gmail.com>
2022-11-07 23:46:58 +0100

    .

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,17 +1,51 @@
 import { useState } from 'react';
 import { Link } from 'react-router-dom';
-import LoginForm from '../components/LoginForm/LoginForm';
 import "./Home.css"
+// import leaderboardlogo from './leaderboard.png';
+import leaderboardIcon from '../images/leaderboard-icon.png'
+
+
 
 
 const Home = () => {
-    return (
-        <>
-<LoginForm/>
-<Link to={"/leaderboard"}>
-<img className='forms sign-in' src={"./images/leaderboard.png"} />
-</Link>
-        </>)
+        const [login, setLogin] = useState("")
+        const [password, setPassword] = useState("")
+
+        const handleSubmit = (event: any) => {
+                event.preventDefault();
+
+                fetch("https://localhost:5000/auth/login", { method: "post", body: JSON.stringify({ login, password }) })
+                //jesli sie udalo przejdz do strony gry, jesli nie "nie udalo sie zalogowac"
+                console.log({ login, password })
+        }
+        return (
+                <>
+                        <form onSubmit={handleSubmit}>
+
+                                <input
+                                        type="text"
+                                        value={login}
+                                        placeholder="Login"
+                                        onChange={(e) => setLogin(e.target.value)}
+                                />
+                                <input
+                                        type="password"
+                                        value={password}
+                                        placeholder="Password"
+                                        onChange={(e) => setPassword(e.target.value)}
+                                />
+                                <button type="submit">
+                                        Login
+                                </button>
+
+                                <Link to="/leaderboard">
+                                        <img src={leaderboardIcon} alt="fireSpot" />
+                                        Leaderboard
+                                </Link>
+
+                        </form>
+
+                </>)
 };
 
 export default Home;
