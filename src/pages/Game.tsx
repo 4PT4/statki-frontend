788e194bb522959e1b02a49baa71f2203c34813f
Radosław Rogalski <80521810+rograd@users.noreply.github.com>
2022-11-11 16:05:41 +0100

    6 strona gry (#21)
    mege '6-strona-gry' into 'main'

---
+++ src/pages/Game.tsx
@@ -0,0 +1,19 @@
+import AllyBoard from '../components/AllyBoard';
+import EnemyBoard from '../components/EnemyBoard';
+import './Game.css';
+
+const Game = () => {
+    return (
+        <div className="game">
+            <h1>Warships game</h1>
+            <h3>Let the battle begin!</h3>
+            <div>
+                <AllyBoard />
+                <button>Start game</button>
+                <EnemyBoard />
+            </div>
+        </div>
+    );
+};
+
+export default Game;
