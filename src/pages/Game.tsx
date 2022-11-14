6846a3c3dbe2cf8de50c38fb563027b4415a881c
vdf <vdf>
2022-11-14 12:09:57 +0100

    status

--- src/pages/Game.tsx
+++ src/pages/Game.tsx
@@ -1,15 +1,36 @@
+import { useEffect, useState } from 'react';
 import AllyBoard from '../components/AllyBoard';
 import EnemyBoard from '../components/EnemyBoard';
 import './Game.css';
 
 const Game = () => {
+    const [status, setStatus] = useState('');
+
+    let socket:WebSocket;
+    const handleReady = (e:any) => {
+        e.preventDefault();
+        socket.send(JSON.stringify({"event":"ready", "data": {}}))
+        
+    }
+    useEffect(() => {
+        socket = new WebSocket('ws://localhost:8000');
+        // Listen for messages
+        socket.addEventListener('message', (event) => {
+            let stat = JSON.parse(event.data);
+            setStatus(stat.data);
+            
+            // { "event": "ready", "data": "" }
+        });
+    });
+
     return (
         <div className="game">
             <h1>Warships game</h1>
             <h3>Let the battle begin!</h3>
+            <p>{status}</p>
             <div>
                 <AllyBoard />
-                <button>Ready</button>
+                <button onClick={handleReady}>Ready</button>
                 <EnemyBoard />
             </div>
         </div>
